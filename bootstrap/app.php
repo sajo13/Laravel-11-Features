52122ed95ca3884cf7daf3433cf00ae6b2bb0931
sajo13 <sajosunny007@gmail.com>
2025-02-05 16:02:37 +0530

    Create custom middleware to log every web request in the app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+
+        $middleware->web(\App\Http\Middleware\LogRequestEndpoint::class);
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
