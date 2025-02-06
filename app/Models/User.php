2579bdc1e3b4e3615af2e15537addfd9d4b6fb7d
sajo13 <sajosunny007@gmail.com>
2025-02-06 11:25:52 +0530

    Custom Casts
    - It converts an attribute to a Collection.
    - The using(SettingsCollection::class) part tells Laravel to use a custom collection class.
    - It also custom cast that allows you to cast an attribute to a collection of enum instances (using Achievement::class).

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\AsCollection;
+use Illuminate\Database\Eloquent\Casts\AsEnumCollection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -45,6 +47,8 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'settings' => AsCollection::using(SettingsCollection::class),
+            'acheivements' => AsEnumCollection::class::of(Acheivement::class),
         ];
     }
 
