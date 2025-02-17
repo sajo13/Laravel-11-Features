b74b1e1009d7e9c32c26e0e66ed1daa284e54212
sajo13 <sajosunny007@gmail.com>
2025-02-17 12:48:41 +0530

    Queue Configure
    - Configure job timeouts and attempts.

--- app/Jobs/SendWelcomeEmail.php
+++ app/Jobs/SendWelcomeEmail.php
@@ -3,12 +3,17 @@
 namespace App\Jobs;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 
 class SendWelcomeEmail implements ShouldQueue
 {
-    use Queueable;
+    use Dispatchable, Queueable, interactsWithQueue, SerializesModels;
 
+    public  $tries = -1;
+    public $backoff = 2;
     /**
      * Create a new job instance.
      */
@@ -22,8 +27,15 @@ class SendWelcomeEmail implements ShouldQueue
      */
     public function handle(): void
     {
+        throw new \Exception('Failed');
+
         sleep(3);
 
         info('Hello!');
+    }
+
+    public function retryUntil()
+    {
+        return now()->addMinutes();
     }
 }
